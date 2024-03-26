7aa3d15c6d567513aa1b354bb0d034108a9ceb19
kuta <150553585+qutasi@users.noreply.github.com>
2024-03-26 10:38:12 +0100

    fetch pokemon locations data

--- pokemon-react/src/App.js
+++ pokemon-react/src/App.js
@@ -1,9 +1,32 @@
 import './App.css';
+import { useEffect, useState } from 'react';
+import Locations from './components/Locations';
+
+
 
 function App() {
+  const [pokemonLocation, setPokemonLocation] = useState(null);
+  
+  useEffect(() => {
+    async function fetchData(pokemonLocation) {
+      const response = await fetch(pokemonLocation)
+      const data = await response.json();
+      setPokemonLocation(data.results)
+      // console.log(data.results);
+      return data.results;
+    }
+    fetchData('https://pokeapi.co/api/v2/location');
+  }, [])
+
+
   return (
     <div className="App">
-      
+      <h1>Pokemon Towns</h1>
+      {pokemonLocation && pokemonLocation.map((location, index) => (
+        <div key={index}>
+          <Locations location={location.name}/>
+        </div>
+      ))}
     </div>
   );
 }
---
+++ pokemon-react/src/components/Locations.js
@@ -0,0 +1,13 @@
+
+import React from 'react'
+
+export default function Locations(props) {
+  const location = props.location;
+  return (
+    <div>
+      <h2>
+      {location}
+      </h2>
+    </div>
+  )
+}
